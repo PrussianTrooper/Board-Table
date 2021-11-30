f68442d2068663fa15881c3e4d61af8f8a22b234
Lukoil39 <goshavasilevskiy@mail.ru>
2021-11-30 14:27:37 +0200

    Добавил меню и прослушку на каждую кнопку

--- app/src/main/java/com/prussian_trooper/tabledenunsionskotlin/learn_project/MainActivity.kt
+++ app/src/main/java/com/prussian_trooper/tabledenunsionskotlin/learn_project/MainActivity.kt
@@ -2,10 +2,58 @@ package com.prussian_trooper.tabledenunsionskotlin.learn_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+    }
+
+    private fun init(){
+        val toogle = ActionBarDrawerToggle(this, drawerLayout, toolbar,R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toogle)
+        toogle.syncState()
+        navView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.id_my_ads -> {
+
+       /*Проверка работоспособности*/
+                Toast.makeText(this, "Pressed id_my_ads", Toast.LENGTH_LONG).show()
+            }
+            R.id.id_car -> {
+                Toast.makeText(this, "Pressed id_car", Toast.LENGTH_LONG).show()
+            }
+            R.id.id_pc -> {
+                Toast.makeText(this, "Pressed id_pc", Toast.LENGTH_LONG).show()
+            }
+            R.id.id_smart -> {
+                Toast.makeText(this, "Pressed id_smart", Toast.LENGTH_LONG).show()
+            }
+            R.id.id_dm -> {
+                Toast.makeText(this, "Pressed id_dm", Toast.LENGTH_LONG).show()
+            }
+            R.id.id_sign_up -> {
+                Toast.makeText(this, "Pressed id_sign_up", Toast.LENGTH_LONG).show()
+            }
+            R.id.id_sing_in -> {
+                Toast.makeText(this, "Pressed id_sing_in", Toast.LENGTH_LONG).show()
+            }
+            R.id.id_sign_out -> {
+                Toast.makeText(this, "Pressed id_sign_out", Toast.LENGTH_LONG).show()
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
